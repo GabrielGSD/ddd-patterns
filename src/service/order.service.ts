1f5fe9b6a8da548dec8b7de2998ab0c7d6b47b93
GabrielGSD <gabrieldaniel@gec.inatel.br>
2023-10-17 11:52:28 -0300

    Definindo politicas de Reward

--- src/service/order.service.ts
+++ src/service/order.service.ts
@@ -1,7 +1,15 @@
 import Order from "../entity/order";
+import Customer from "../entity/customer";
+import OrderItem from "../entity/order_item";
 
 export default class OrderService {
     static total(orders: Order[]): number {
         return orders.reduce((acc, order) => acc + order.total(), 0);
+    }
+
+    static placeOrder(customer: Customer, items: OrderItem[], id: string): Order {
+        const order = new Order(id, customer.id, items);
+        customer.activate();
+        return order;
     }
 }
